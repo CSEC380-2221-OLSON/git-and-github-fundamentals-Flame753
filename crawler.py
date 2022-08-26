92885be305629e3738281e931cee6796e63764ae
Flame753 <Sgurman753@gmail.com>
2022-08-26 18:18:45 -0400

    Got a way to obtain URLs from a tags

--- crawler.py
+++ crawler.py
@@ -1,7 +1,9 @@
 import sys
+from lxml import etree, html
+import requests
 
-class Scraper:
-     pass
+
+
 
 def get_command_args():
     if (len(sys.argv)-1) < 3:
@@ -11,6 +13,19 @@ def get_command_args():
     return sys.argv[1:4]
 
 
-if __name__ == "__main__":
+def get_a_tags_urls(url) -> list:
+    response = requests.get("https://www.rit.edu/")
+    tree = html.fromstring(html=response.text)
+    return tree.xpath('//a/@href')
+
+
+class Scraper:
+     pass
+
+
+def main():
     domain, url, depth= get_command_args()
     print(domain, url, depth)
+
+if __name__ == "__main__":
+    main()

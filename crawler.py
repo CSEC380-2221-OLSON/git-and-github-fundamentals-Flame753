ab076728b975e2cec67c56c6eb08cb64821877d5
Flame753 <Sgurman753@gmail.com>
2022-08-26 18:25:14 -0400

    combined both function outputs into one main function

--- crawler.py
+++ crawler.py
@@ -1,11 +1,10 @@
 import sys
-from lxml import etree, html
+from lxml import html
 import requests
 
+#python crawler.py rit.edu https://www.rit.edu/ 1
 
-
-
-def get_command_args():
+def get_command_args() -> list:
     if (len(sys.argv)-1) < 3:
         # Stopping the script before breaking something
         sys.exit()
@@ -13,19 +12,17 @@ def get_command_args():
     return sys.argv[1:4]
 
 
-def get_a_tags_urls(url) -> list:
-    response = requests.get("https://www.rit.edu/")
+def get_urls_a_tags(url:str) -> list:
+    response = requests.get(url)
     tree = html.fromstring(html=response.text)
     return tree.xpath('//a/@href')
 
 
-class Scraper:
-     pass
-
-
 def main():
     domain, url, depth= get_command_args()
-    print(domain, url, depth)
+    urls = get_urls_a_tags(url)
+    print(urls)
+
 
 if __name__ == "__main__":
     main()

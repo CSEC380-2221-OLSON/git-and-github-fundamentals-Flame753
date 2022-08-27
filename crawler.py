8fa08592864a076802709debab523d7d9c94cc62
Flame753 <Sgurman753@gmail.com>
2022-08-27 01:02:38 -0400

    3rd time updating how to filter the urls. This version looks at the list of urls only once.

--- crawler.py
+++ crawler.py
@@ -1,7 +1,9 @@
+from lib2to3.pgen2.token import DOUBLESLASH
 import sys
 from lxml import html
 import requests
 import re
+import enum
 from pprint import pprint
 
 #python crawler.py rit.edu https://www.rit.edu/ 1
@@ -32,41 +34,30 @@ def remove_duplicates(urls:list[str]) -> list[str]:
     return list(set(urls))
 
 
-def filtering_out_urls(urls:list[str], prefixs_filter:str) -> list[str]:
-    new_urls_list = list()
+class URL(enum.Enum):
+    HTTPS = "https://"
+    HTTP = "http://"
+    DOUBLESLASH = "//"
+    SINGLESLASH = "/"
+    EXTENSIONS = ""
+
+def filtering_urls(urls:list[str], prefixs_filter:list[str]) -> dict[str: list[str]]:
+    url_dict = {type.name: list() for type in URL}
+
     for url in urls:
-        if has_prefix(url, prefixs_filter):
-            new_urls_list.append(url)
-    return new_urls_list
-
-
-def url_extension_spliter(urls:list[str]) -> dict[str:list[str]]:
-
-    https_urls = filtering_out_urls(urls, prefixs_filter="https://")
-    urls_without_https = [url for url in urls if url not in https_urls]
-
-    http_urls = (filtering_out_urls(urls_without_https, prefixs_filter="http://"))
-    urls_without_http = [url for url in urls_without_https if url not in http_urls]
-
-    double_slash_urls = (filtering_out_urls(urls_without_http, prefixs_filter="//"))
-    urls_without_double_slashes = [url for url in urls_without_http if url not in double_slash_urls]
-
-    single_slash_extensions = (filtering_out_urls(urls_without_double_slashes, prefixs_filter="/"))
-    extensions_without_single_slashes = [url for url in urls_without_double_slashes if url not in single_slash_extensions]
-    
-    return {"https_urls": https_urls, 
-            "http_urls": http_urls,
-            "double_slash_urls": double_slash_urls,
-            "single_slash_extensions": single_slash_extensions,
-            "extensions": extensions_without_single_slashes}
-
+        for prefix in prefixs_filter:
+            if has_prefix(url, prefix.value):
+                urls_list = url_dict.get(prefix.name)
+                urls_list.append(url)
+                continue
+    return url_dict
 
 
 def main():
     domain, url, depth= get_command_args()
     orig_urls = get_urls_a_tags(url)
     no_dup_urls = remove_duplicates(orig_urls)
-    url_dict = url_extension_spliter(no_dup_urls)
+    url_dict = filtering_urls(no_dup_urls, [URL.HTTPS, URL.HTTP, URL.DOUBLESLASH, URL.SINGLESLASH])
     pprint(url_dict)
 
 

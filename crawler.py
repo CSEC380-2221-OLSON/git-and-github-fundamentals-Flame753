9ca2e60b8da822c0cf2a07f3e74bd2053234ed26
Flame753 <Sgurman753@gmail.com>
2022-08-26 20:56:12 -0400

    Created a way to remove url duplcates and separate urls from extensions

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ import sys
 from lxml import html
 import requests
 import re
+from pprint import pprint
 
 #python crawler.py rit.edu https://www.rit.edu/ 1
 
@@ -30,11 +31,24 @@ def filtering_out_bad_urls(urls:list[str], good_urls_features:list[str]) -> list
                 continue
     return new_urls_list
 
+
+def remove_duplicates(urls:list[str]) -> list[str]:
+    return list(set(urls))
+
+
+def url_extension_spliter(urls:list[str]) -> dict[str:list[str]]:
+    urls_with_domain = filtering_out_bad_urls(urls, good_urls_features=["http", "//"])
+    extensions = [url for url in urls if url not in urls_with_domain]
+    return {"urls": urls_with_domain, "extensions": extensions}
+
+
 def main():
     domain, url, depth= get_command_args()
-    urls = get_urls_a_tags(url)
-    filtered_urls = filtering_out_bad_urls(urls, good_urls_features=['/', 'http'])
-    print(filtered_urls)
+    orig_urls = get_urls_a_tags(url)
+    no_dup_urls = remove_duplicates(orig_urls)
+    print(no_dup_urls)
+    url_dict = url_extension_spliter(no_dup_urls)
+    pprint(url_dict)
 
 
 if __name__ == "__main__":

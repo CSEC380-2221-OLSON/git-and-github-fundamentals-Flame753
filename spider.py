48de22f4ebc4ee7ef5554d394f6653fffc7d5548
Flame753 <Sgurman753@gmail.com>
2022-08-28 12:38:16 -0400

    Redsesign 2

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 from pprint import pprint
 import re
 import requests
+import time
 
 
 # python spider.py www.rit.edu https://www.rit.edu/ 1
@@ -22,25 +23,21 @@ def get_command_args() -> list[str]:
     return sys.argv[1:4]
 
 def create_url(scheme, domain, current_path, url):
-    # print(f"current_path: {current_path}", f"url: {url}", "Are both the same:",current_path == url)
+    urldata = urlparse(url)
+
+    if urldata.scheme not in ['', 'http', 'https']:
+        return url
+
     if url.startswith('/'):
         url = f'{scheme}://{domain}{url}'
     elif not url.startswith('http'):
-        url = f'{scheme}://{domain}{current_path}/{url}'
+        if urldata.path == '' and urldata.fragment != '':
+            url = f'{scheme}://{domain}{current_path}#{urldata.fragment}'
+        else:
+            url = f'{scheme}://{domain}{current_path}/{url}'
     return url
 
-def get_resources(content: str) -> set[str]:
-    tags = {'a': 'href', 'link': 'href', 'img': 'src', 'script': 'src', 'iframe': 'src', 'object': 'data' }
-    links = []
-    tree = bs(content, 'html.parser')
-    for tag, value in tags.items():
-        for link in tree.find_all(tag):
-            url_path = link.get(value)
-            if url_path:  # Preventing None values to be added 
-                links.append(url_path)
-    return set(links)
-
-def spider(domain, url, depth=0):
+def get_resources(domain, url, depth=0) -> set[str]:
     urldata = urlparse(url)
 
     # if domain doesnt match then skip
@@ -57,23 +54,34 @@ def spider(domain, url, depth=0):
     # if depth has reached 0 then go no further
     if depth == 0:
         return False
-    
-    print(url)
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        print(resp)
-        # if resp.status_code:
 
-        links = get_resources(resp.text)
-        for link in links:
-            spider(domain, create_url(urldata.scheme, domain, urldata.path, link), depth - 1)
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+
+        tags = {'a': 'href', 'link': 'href', 'img': 'src', 'script': 'src', 'iframe': 'src', 'object': 'data' }
+        tree = bs(resp.text, 'html.parser')
+        for tag, value in tags.items():
+            for link in tree.find_all(tag):
+                url_path = link.get(value)
+                if url_path:  # Preventing None values to be added
+                    get_resources(domain, create_url(urldata.scheme, domain, urldata.path, url_path), depth - 1)
+
+
+def write_resources(filepath):
+    with open(filepath, 'w') as fh:
+        for url in urls:
+            fh.write(f'{url}\n')
 
 
 def main():
     domain, url, depth= get_command_args()
-    spider(domain, url, int(depth))
-    pprint(urls)
+    get_resources(domain, url, int(depth))
+    write_resources('urls-gathered')
     
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    lapsed = time.time() - start
+    print(lapsed)
